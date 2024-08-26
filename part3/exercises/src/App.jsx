b3beb8f7ef35f79bac30c92688e2f386d3f77444
Maeganbcode <hairstylistmae@gmail.com>
2024-08-25 23:48:31 -0500

    My-Hobby-Exercise

--- part3/exercises/src/App.jsx
+++ part3/exercises/src/App.jsx
@@ -7,11 +7,11 @@ function App() {
 
   return (
     <>
-      <div className="App">
-	  <h1>My Hobby: Needlepoint</h1>
-	  <HobbyIntroduction />
-	  <MyProjects />
-      </div>
+<div className="App">
+   <h1>My Hobby: Crocheting</h1>
+   <HobbyIntroduction />
+   <MyProjects />
+</div>
     </>
   )
 }
--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Crocheting</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Crochet comes from "croche", the Middle French word for hook.</li>
+            <li>Crocheting reduces stress and anxiety.</li>
+            <li>“Yarn bombing” is a form of street art where crocheters and knitters cover public objects like lampposts and trees with colorful yarn.</li>
          </ol>
       </div>
    );
